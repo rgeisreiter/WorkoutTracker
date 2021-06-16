08a9a140b77978f949887197cbda8acdb19a492e
rgeisreiter <rgeisreiter@yahoo.com>
2021-06-16 15:31:59 -0600

    created schema

--- models/workout.js
+++ models/workout.js
@@ -2,49 +2,9 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema(
-  {
-    day: {
-      type: Date,
-      default: () => new Date(),
-    },
-    exercises: [
-      {
-        type: {
-          type: String,
-          trim: true,
-          required: "Enter an exercise",
-        },
-        name: {
-          type: String,
-          trim: true,
-          required: "Enter an exercise",
-        },
-        length: {
-          type: Number,
-          required: "Enter excersize length",
-        },
-        weight: {
-          type: Number,
-        },
-        reps: {
-          type: Number,
-        },
-        sets: {
-          type: Number,
-        },
-        distance: {
-          type: Number,
-        },
-      },
-    ],
-  },
-  {
-    toJSON: {
-      virtuals: true,
-    },
-  }
-);
+const workoutSchema = new Schema({
+  day: { type: Date, default: () => new Date() },
+});
 
 const Workout = mongoose.model("Workout", workoutSchema);
 
